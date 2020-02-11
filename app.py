016295ecda83a4dea637e194876878309e222028
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-02-11 11:53:47 -0500

    changes

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[86]:
+
+
 from bs4 import BeautifulSoup
 import pandas as pd
 import glob
 
+
+# In[87]:
+
+
 def get_df_from_filename(filename):
     soup = BeautifulSoup(open(filename), "html.parser")
     items = []
-    for item in soup.find_all('div', {'class' : 'PH-ProductCard-product-info flex content-start justify-end'}):
+    for item in soup.find_all('div', {'class' : 'PH-ProductCard-description flex justify-between flex-grow'}):
         tag = item.find('div', {'class': 'flex flex-col pl-16'})
         items.append([x for x in tag.strings] + [item.find('span', {'class': 'PH-ProductCard-Total'}).string])
-        df = pd.DataFrame(items)
+    df = pd.DataFrame(items)
     df[2] = df[2] + df[3] + df[4]
     df = df[[0,1,2,5]].rename({0: 'Item', 1:'Selling Quantity', 2:'Price per Quantity', 5: 'Price'}, axis = 1)
     return df
+
+
+# In[88]:
+
 
 def get_csv(htmls, csvs):
     html_files = glob.glob(htmls)
@@ -26,6 +40,17 @@ def get_csv(htmls, csvs):
         else:
             print('{}.csv already exists'.format(file_name))
 
+
+# In[89]:
+
+
 htmls = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.html'
 csvs = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.csv'
 get_csv(htmls, csvs)
+
+
+# In[ ]:
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,3 @@
+jupyter nbconvert --to script  GroceryScraperV3.ipynb
+mv GroceryScraperV3.py app.py
+python app.py
