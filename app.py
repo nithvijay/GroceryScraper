ca7a14d2efb8cef4f8a559257ab80ee4a8d85453
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-05-20 15:46:56 -0400

    from new run.sh

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[86]:
-
-
 from bs4 import BeautifulSoup
 import pandas as pd
 import glob
+import re
 
 
-# In[87]:
+ITEM_CLASS = r"PH-ProductCard-container"
+DESCRIPTION_CLASS = r"flex flex-col pl-16"
+PRICE_CLASS = r"PH-ProductCard-Total"
 
 
 def get_df_from_filename(filename):
     soup = BeautifulSoup(open(filename), "html.parser")
     items = []
-    for item in soup.find_all('div', {'class' : 'PH-ProductCard-description flex justify-between flex-grow'}):
-        tag = item.find('div', {'class': 'flex flex-col pl-16'})
-        items.append([x for x in tag.strings] + [item.find('span', {'class': 'PH-ProductCard-Total'}).string])
+    for item in soup.find_all('article', {'class' : ITEM_CLASS}):
+        tag = item.find('div', {'class': re.compile(DESCRIPTION_CLASS + ".*")})
+        items.append([x for x in tag.strings] + [item.find('span', {'class': re.compile(PRICE_CLASS + ".*")}).string])
     df = pd.DataFrame(items)
     df[2] = df[2] + df[3] + df[4]
     df = df[[0,1,2,5]].rename({0: 'Item', 1:'Selling Quantity', 2:'Price per Quantity', 5: 'Price'}, axis = 1)
     return df
-
-
-# In[88]:
 
 
 def get_csv(htmls, csvs):
@@ -41,15 +38,9 @@ def get_csv(htmls, csvs):
             print('{}.csv already exists'.format(file_name))
 
 
-# In[89]:
-
-
 htmls = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.html'
 csvs = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.csv'
 get_csv(htmls, csvs)
-
-
-# In[ ]:
 
 
 
