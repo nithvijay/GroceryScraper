65b795eeccefdb567350e5fbc46760edb8520c1d
Nithin Vijayakumar <nv2ba@virginia.edu>
2020-05-20 15:46:41 -0400

    added argument to nbconvert to remove input cells

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-jupyter nbconvert --to script  GroceryScraperV3.ipynb
+jupyter nbconvert --to script  GroceryScraperV3.ipynb --TemplateExporter.exclude_input_prompt=True
 mv GroceryScraperV3.py app.py
 python app.py
