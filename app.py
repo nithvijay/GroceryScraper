71bdbbfc1413fccb8cda762c765c82e8b55d8f22
Nithin Vijayakumar <nv2ba@virginia.edu>
2021-02-04 00:16:33 -0500

    latest

--- app.py
+++ app.py
@@ -4,29 +4,39 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import glob
-import re
 
 
-ITEM_CLASS = r"PH-ProductCard-container"
-DESCRIPTION_CLASS = r"flex flex-col pl-16"
-PRICE_CLASS = r"PH-ProductCard-Total"
+def get_price(soup):
+    item = soup.find('div', {'class': "PH-ProductCard-Total"})
+    return item.find('data')['value'].strip()
 
+def get_description(soup):
+    item = soup.find('span', {'class': "PH-ProductCard-item-description"}) 
+    return " ".join(item.string.split())
+
+def get_quantity(soup):
+    item = soup.find('div', {'class': "PH-ProductCard-Total"})
+    return " ".join([i.strip() for i in item.find('span', {'kds-Text--s'}).strings][:3])
+
+def get_selling_quantity(soup):
+    item = soup.find('span', {'class': 'kds-Text--xs'})
+    return " ".join(item.string.split())
 
 def get_df_from_filename(filename):
     soup = BeautifulSoup(open(filename), "html.parser")
     items = []
-    for item in soup.find_all('article', {'class' : ITEM_CLASS}):
-        tag = item.find('div', {'class': re.compile(DESCRIPTION_CLASS + ".*")})
-        items.append([x for x in tag.strings] + [item.find('span', {'class': re.compile(PRICE_CLASS + ".*")}).string])
-    df = pd.DataFrame(items)
-    df[2] = df[2] + df[3] + df[4]
-    df = df[[0,1,2,5]].rename({0: 'Item', 1:'Selling Quantity', 2:'Price per Quantity', 5: 'Price'}, axis = 1)
-    return df
-
+    for product in soup.find_all("div", {'class': 'PH-ProductCard-productInfo'}):
+        item = {}
+        item['Price'] = get_price(product)
+        item['Item'] = get_description(product)
+        item['Price per Quantity'] = get_quantity(product)
+        item['Selling Quantity'] = get_selling_quantity(product)
+        items.append(item)
+    return pd.DataFrame(items)[['Item', 'Selling Quantity','Price per Quantity', 'Price']]
 
 def get_csv(htmls, csvs):
     html_files = glob.glob(htmls)
-    csv_files = pd.Series(glob.glob(csvs))
+    csv_files = pd.Series(glob.glob(csvs), dtype='object')
     for html_file in html_files:
         start_index = html_file.rindex('/') + 1
         end_index = html_file.rindex('.')
@@ -41,7 +51,4 @@ def get_csv(htmls, csvs):
 htmls = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.html'
 csvs = '/Users/nvijayakumar/OneDrive/School/College/Misc/Grocery/*.csv'
 get_csv(htmls, csvs)
-
-
-
 
